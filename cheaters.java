4ccd3a38009aedb8315d33f56914049fe809f91b
Christine <daochristine039@utexas.edu>
2018-04-28 19:26:40 -0500

    Works properly (commands not implemented)

--- cheaters.java
+++ cheaters.java
@@ -2,33 +2,40 @@ package assignment7;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class cheaters {
-    //private Map<String, Scanner> files = new LinkedHashMap<>();
-    private Map<Integer, String[]> comparisons = new LinkedHashMap<>();
+    private static Map<String, ArrayList<String>> sequences = new LinkedHashMap<>();
+    //private static Map<String, String> toCompare = new LinkedHashMap<>();
+    private static Map<String[], Integer> comparisons = new LinkedHashMap<>();
 
 
     public static void main(String[] args) {
-        Map<String, String[]> files = makeFiles();
-        compareFiles(files);
-
+        long startTime = System.nanoTime();
+        makeFiles();
+        compareFiles();
+        printComparisons();
+        long endTime = System.nanoTime();
+        System.out.println("Took "+(endTime - startTime) + " ns");
 
     }
 
-    public static Map<String, String[]> makeFiles() {
+    public static void makeFiles() {
         Map<String, String[]> files = new LinkedHashMap<>();
-        File dir = new File("C:\\Users\\hvu\\Desktop\\Project7\\sm_doc_set");
+        File dir = new File("C:\\Users\\hvu\\Desktop\\Project7\\big_doc_set");
 
 
         //File[] listFiles = dir.listFiles();
 
         for (File file : dir.listFiles()) {
-            String temp = "";
+            String temp = "";                   //To get array of words
+//            String temp1 = "";                  //To get a string without spaces and punctuation
             StringBuilder result = new StringBuilder();
+//            StringBuilder result1 = new StringBuilder();
             if(file.isFile()) {
                 try {
                     temp = new String(Files.readAllBytes(Paths.get(file.toString())));
@@ -41,37 +48,167 @@ public class cheaters {
                     if(((temp.charAt(i) >= 'a' && temp.charAt(i) <= 'z') || temp.charAt(i) == ' ') || (temp.charAt(i) >= '0' && temp.charAt(i) <= '9')){
                         result.append(temp.charAt(i));
                     }
+
+//                    if((temp.charAt(i) >= 'a' && temp.charAt(i) <= 'z') || (temp.charAt(i) >= '0' && temp.charAt(i) <= '9')){
+//                        result1.append(temp.charAt(i));
+//                    }
                 }
 
                 temp = "";
                 temp = temp + result;
+//                temp1 = temp1 + result1;
                 String[] wordsArray = temp.split(" ");
                 files.put(file.getName(), wordsArray);
+//                toCompare.put(file.getName(), temp1);
             }
         }
 
         if(files.isEmpty()){
-            return null;
+            return;
         }
 
-        return files;
-
-    }
-
-    public static void compareFiles(Map<String, String[]> files){
         Iterator iterator = files.keySet().iterator();
         String key = "";
+        String temp = "";
 
         while(iterator.hasNext()){
-            String newValue = "";
+            ArrayList<String> tempArr = new ArrayList<>();
             key = (String) iterator.next();
             String[] currentValue = files.get(key);
-            int count = 0;  //Keeps track of which word to start with
+            int count = 0;
 
-            for(int i = 0, j = count; ; i++, j++){
-                newValue += currentValue[j];
+            loop1: for(; ; count++) {
+                temp = "";
+                for (int i = 0, j = count; i < 6; i++, j++) {   //i = checks for 6-word-sequence
+                    if (j == currentValue.length) {
+                        break loop1;
+                    }
+                    temp += currentValue[j];
+                }
+
+                tempArr.add(temp);
             }
 
-
+            sequences.put(key, tempArr);
         }
     }
+
+    public static void compareFiles(){
+        ArrayList<String> current = new ArrayList<>();
+        ArrayList<String> toCmp = new ArrayList<>();
+
+        Iterator iterator = sequences.keySet().iterator();
+        String key = "";
+        String key1 = "";
+        int numOfRep = 0;
+
+        while(iterator.hasNext()){
+            key = (String) iterator.next();
+            current = sequences.get(key);
+
+            Iterator cmpIterator = sequences.keySet().iterator();
+
+            while(cmpIterator.hasNext()){
+                key1 = (String) cmpIterator.next();
+                if(key1.equals(key)){
+                    continue;
+                }
+                toCmp = sequences.get(key1);
+
+                for(String sixWords : current){
+                    if(toCmp.contains(sixWords)){
+                        numOfRep++;
+                    }
+                }
+
+                if(numOfRep > 200){
+                    String[] temp1 = {key, key1};
+                    if(!comparisons.containsKey(temp1)){
+                        comparisons.put(temp1, numOfRep);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printComparisons(){
+        List<Map.Entry<String[], Integer>> list = new ArrayList<>(comparisons.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+        Map<String[], Integer> result = new LinkedHashMap<>();
+        for(Map.Entry<String[], Integer> entry : list){
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        Iterator iterator = result.keySet().iterator();
+        ArrayList keyList = new ArrayList<>(result.keySet());
+        for(int i = result.size() - 1; i >= 0; i--){
+            String[] key = (String[]) keyList.get(i);
+            int value = result.get(key);
+            System.out.print(value + ": ");
+
+            for(int j = 0; j < key.length; j++){
+                if(j == key.length - 1){
+                    System.out.println(key[j]);
+                } else {
+                    System.out.print(key[j] + ", ");
+                }
+            }
+
+        }
+
+    }
+}
+
+//    public static void compareFiles(Map<String, String[]> files){
+//        Iterator iterator = files.keySet().iterator();
+//        String key = "";            //File name
+//        String key1 = "";
+//
+//        while(iterator.hasNext()){
+//            String newValue = "";       //current 6-word-sequence
+//
+//            Iterator fileToCmp = toCompare.keySet().iterator();
+//            String strToCmp = "";
+//
+//            key = (String) iterator.next();
+//            String[] currentValue = files.get(key);     //File content
+//
+//            while(fileToCmp.hasNext()) {
+//                int count = 0;  //Keeps track of which word to start with
+//                int numOfRep = 0;
+//                key1 = (String) fileToCmp.next();
+//                if(key1.equals(key)){
+//                    continue;
+//                }
+//                strToCmp = toCompare.get(key1);
+//
+//                loop1:
+//                for (; ; count++) {
+//                    newValue = "";
+//                    for (int i = 0, j = count; i < 6; i++, j++) {   //i = checks for 6-word-sequence
+//                        if (j == currentValue.length) {
+//                            break loop1;
+//                        }
+//                        newValue += currentValue[j];
+//                    }
+//
+//                    for (int i = 0, j = newValue.length(); ; i++, j++) {
+//                        if (j == strToCmp.length() + 1) {
+//                            break;
+//                        }
+//                        String temp = strToCmp.substring(i, j);
+//
+//                        if (temp.equals(newValue)) {
+//                            numOfRep++;
+//                        }
+//                    }
+//                }
+//
+//                if(numOfRep >= 200){                    //FIX 200 TO ANY NUMBER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                    comparisons.put(numOfRep, key);
+//                    comparisons.put(numOfRep, key1);
+//                }
+//            }
+//        }
+//    }
